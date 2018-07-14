a04b743ac3488345c01f60d410b1677bb4f7288e
GhiaC <ghiasy@aut.ac.ir>
2018-07-14 15:26:22 +0430

    change port

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -122,7 +122,7 @@ object WebServer {
       }
 
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 80)
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
